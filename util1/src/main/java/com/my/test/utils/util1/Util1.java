5faea048c5bc578bbbb9ba0131ff86509fc4026d
minichen <minichen@qq.com>
2016-10-01 19:50:38 +0800

    ..

---
+++ util1/src/main/java/com/my/test/utils/util1/Util1.java
@@ -0,0 +1,10 @@
+package com.my.test.utils.util1;
+
+/**
+ * Created by minichen on 16/10/1.
+ */
+public class Util1 {
+    public static void sayHello(){
+        System.out.print("Hello");
+    }
+}
---
+++ util2/src/main/java/com/my/test/utils/util2/Util2.java
@@ -0,0 +1,10 @@
+package com.my.test.utils.util2;
+
+/**
+ * Created by minichen on 16/10/1.
+ */
+public class Util2 {
+    public static void sayWorld(){
+        System.out.print("World");
+    }
+}
